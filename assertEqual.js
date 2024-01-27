73fd67663edc1e185919e2dd0c50d2eacef75d7d
Krisan Manoharan <krisan1010@hotmail.com>
2024-01-26 21:00:29 -0500

    emoji

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("Assertion Passed: " + [actual] + " === " + [expected]);
-  } else {
-    console.log("Assertion Failed: " + [actual] + " !== " + [expected]);
+    console.log(`🤑🤑🤑 Assertion Passed: ${actual} === ${expected}`)} else {
+    console.log( `😂😂😂 Assertion Failed: ${actual} !== ${expected}`)
   }
 };
 assertEqual(3, 1);
+assertEqual(2, 2);
