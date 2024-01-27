97fab3cf8c69b7820d34aecdf507386c680cd82f
Krisan Manoharan <krisan1010@hotmail.com>
2024-01-26 21:53:59 -0500

    Updated Head

--- head.js
+++ head.js
@@ -12,4 +12,5 @@ const head = function(array) {
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([1]), 1);
-assertEqual(head([]), undefined);
+assertEqual(head([1]), 2);
+assertEqual(head([]), undefined); 
