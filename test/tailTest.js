5536a3cba3b78902b1ca43d4e4acc346148de9b3
Krisan Manoharan <krisan1010@hotmail.com>
2024-02-16 20:21:41 -0500

    Added tailTest

---
+++ test/tailTest.js
@@ -0,0 +1,15 @@
+const tail = require('../tail')
+const assertEqual = require('../assertEqual');
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+console.log(tail(words)); 
+assertEqual(tail(words).length, 2); 
+
+
+const pokemon = ["Pikachu", "Rampardos", "Tsareena", "Flamigo"];
+console.log(tail(pokemon));
+assertEqual(tail(pokemon).length, 3);
+
+const digimon = ["Angewomon"]
+console.log(tail(digimon));
+assertEqual(tail(digimon).length, 0)
