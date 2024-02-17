cd16dae8fd0a7a07094d52fd98f4d7f48bf3bb64
Krisan Manoharan <krisan1010@hotmail.com>
2024-02-16 19:53:49 -0500

    Edited assertEqual and head

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,11 @@
 // This function compares two types and checks to see if they match. Strings and numbers will always be different.
 
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
+  if (actual === expected) { // This line takes the two parameters to check if the are the exact same.
     console.log(`🤑🤑🤑 Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`😂😂😂 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-assertEqual(3, 1);
-assertEqual(2, "2");
-assertEqual(true, true);
+
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,19 +1,9 @@
 //This function will take the first value of an array.
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🤑🤑🤑 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😂😂😂 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
 const head = function(array) {
   return array[0];
 };
 
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
-assertEqual(head([1]), 2);
-assertEqual(head([]), undefined);
+module.exports = head;
