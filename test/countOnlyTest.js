1ee7d44872c18bad916c1e635a3628ab003aabb1
Krisan Manoharan <krisan1010@hotmail.com>
2024-02-17 10:17:11 -0500

    Added tests to countOnlyTest

--- test/countOnlyTest.js
+++ test/countOnlyTest.js
@@ -1,5 +1,25 @@
 const countOnly = require('../countOnly')
 const assertEqual = require('../assertEqual')
+const assert = require('chai').assert;
+
+describe("#countOnly", () => {
+  it("returns 2 when counting Metagross for [Lopunny, Slaking, Honchkrow, Metagross, Metagross, Florges]", () => {
+    assert.deepEqual(countOnly(["Metagross", "Slaking", "Clefairy", "Metagross", "Rhyperior"], {Metagross: true}), {Metagross: 2});
+  });
+
+  it("returns {} when not counting Graveler for [Golurk, Golduck, Goldeen, Gravler, Gholdengo, Graveler", () => {
+    assert.deepEqual(countOnly (["Golurk", "Golduck", "Graveler", "Gholdengo", "Graveler"], {Graveler: false}), {})
+  });
+
+it("returns {} when counting Wurmple for [Geodude, Zubat, Aron, Makuhita]", () => {
+  assert.deepEqual(countOnly(["Geodude","Zubat", "Aron", "Makuhita", "Zubat"], {Wurmple: true}), {})
+})
+
+it("returns {} when not counting Trapinch for [Tropius, Absol, Linoone, Linoone, Keckleon]", () => {
+assert.deepEqual(countOnly(["Keckleon", "Tropius", "Linoone", "Absol", "Linoone"], {Trapinch: false}), {})
+})
+});
+
 
 const firstNames = [
   "Karl",
@@ -15,7 +35,4 @@ const firstNames = [
 
 const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
 
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+console.log(countOnly(firstNames, {Fang: false}))
